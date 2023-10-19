9b27341fc083660ec95724e64556c082d918d95d
Azamat <sch_azamat@mail.ru>
2023-10-19 19:57:50 +0500

    created Toys class

---
+++ Toys.java
@@ -0,0 +1,11 @@
+public class Toys {
+    int id;
+    String name;
+    int weight;
+
+    public Toys (int id, String name, int weight){
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+}
